9eb4c0ea67ed5796c33c11f1929707f379a5207d
tahentx <hendricks.ta@gmail.com>
2018-08-16 20:28:05 -0700

    import salesforce as sf; import work order csv

--- truckroll.py
+++ truckroll.py
@@ -1,1 +1,3 @@
 import numpy as np
+cost = input("What is your truck roll cost?")
+budget = input("What is your annual O&M budget?")
--- workorder.py
+++ workorder.py
@@ -1,7 +1,9 @@
-import simple_salesforce
-
+import simple_salesforce as sf
+import numpy as np
 wo = ''
 source = ''
+
+np.genfromtext("workorder.csv", delimeter=',')
 
 print("test")
 
