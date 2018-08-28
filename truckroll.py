e06999d4af9c5994fd43154f5d451ffbb63272bb
tahentx <hendricks.ta@gmail.com>
2018-08-27 20:06:48 -0700

    add daily cost increment

--- truckroll.py
+++ truckroll.py
@@ -2,4 +2,6 @@ import numpy as np
 cost = input("What is your truck roll cost?")
 budget = input("What is your annual O&M budget?")
 ppa_price = .1
-
+while cost < budget:
+	cost += cost
+	print(cost)
