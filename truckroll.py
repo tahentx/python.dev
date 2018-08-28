ea863e09052f192bb672a9d77cdcc555d82df72d
tahentx <hendricks.ta@gmail.com>
2018-08-27 20:14:45 -0700

    add break statement

--- truckroll.py
+++ truckroll.py
@@ -1,7 +1,11 @@
 import numpy as np
 cost = input("What is your truck roll cost?")
 budget = input("What is your annual O&M budget?")
-ppa_price = .1
+def ppa_rate:
+	placeholder
 while cost < budget:
 	cost += cost
 	print(cost)
+	if cost == budget:
+	break
+	print("It makes economic sense to roll a truck.")
