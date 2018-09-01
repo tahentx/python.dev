5578112549a3a5905fe1120db948232ef3e4467f
tahentx <hendricks.ta@gmail.com>
2018-08-31 18:28:15 -0700

    remove previous code for testing purposes

--- truckroll.py
+++ truckroll.py
@@ -1,11 +1,12 @@
 import numpy as np
 cost = input("What is your truck roll cost?")
 budget = input("What is your annual O&M budget?")
-def ppa_rate:
-	placeholder
-while cost < ppa_rate:
-	cost += cost
-	print(cost)
-	if cost == ppa_rate:
-	break
-	print("It makes economic sense to roll a truck.")
+
+# def ppa_rate():
+	
+# while cost < ppa_rate:
+# 	cost += cost
+# 	print(cost)
+# 	if cost == ppa_rate:
+# 	break
+# 	print("It makes economic sense to roll a truck.")
