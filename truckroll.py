11c1f9cb4aa99f74ad576ffc367e1db2bccaff42
tahentx <hendricks.ta@gmail.com>
2018-08-31 19:30:55 -0700

    working to return loss calculation and convert to integer; will come back to it after dinner

--- truckroll.py
+++ truckroll.py
@@ -1,4 +1,4 @@
-# cost = input("What is your truck roll cost?")
+cost = input("What is your truck roll cost?")
 # budget = input("What is your annual O&M budget?")
 # PPA units in kWh
 ppa = 0.15
@@ -7,12 +7,15 @@ ppa = 0.15
 # plant_ac_capacity = 744
 energy_billable = 4000
 def daily_loss(ppa,energy_billable):
-	print(ppa * energy_billable)
+	loss_calculation = ppa * energy_billable
+	return loss_calculation
+	print(loss_calculation)
 daily_loss(ppa,energy_billable)
-	
-while cost > daily_loss:
-	cost += cost
-	print(cost)
+
+
+# while int(cost) > int(daily_loss):
+# 	daily_loss += daily_loss
+# 	print(daily_loss)
 	# if cost == ppa_rate:
 	# break
 	# print("It makes economic sense to roll a truck.")
