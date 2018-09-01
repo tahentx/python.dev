fe1fb7f7a44b17776902555d80e16d29550687b0
tahentx <hendricks.ta@gmail.com>
2018-08-31 18:45:11 -0700

    add nameplate ratings

--- truckroll.py
+++ truckroll.py
@@ -1,7 +1,12 @@
 cost = input("What is your truck roll cost?")
 budget = input("What is your annual O&M budget?")
+# PPA units in kWh
+ppa = .15
+# Plant capacity in kw
+plant_dc_capacity = 996.48
+plant_ac_capacity = 744
 
-# def ppa_rate():
+
 	
 # while cost < ppa_rate:
 # 	cost += cost
