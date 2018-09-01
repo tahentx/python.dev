d0cf776cd4a3ed80efc0c52d9ce4f6a56fa08767
tahentx <hendricks.ta@gmail.com>
2018-08-31 19:09:46 -0700

    define daily loss function as ppa rate * energy billable. these values are arbitrary right now. will make dynamic later.

--- truckroll.py
+++ truckroll.py
@@ -1,16 +1,18 @@
-cost = input("What is your truck roll cost?")
-budget = input("What is your annual O&M budget?")
+# cost = input("What is your truck roll cost?")
+# budget = input("What is your annual O&M budget?")
 # PPA units in kWh
-ppa = .15
+ppa = 0.15
 # Plant capacity in kw
-plant_dc_capacity = 996.48
-plant_ac_capacity = 744
-
-
+# plant_dc_capacity = 996.48
+# plant_ac_capacity = 744
+energy_billable = 4000
+def daily_loss(ppa,energy_billable):
+	print(ppa * energy_billable)
+daily_loss(ppa,energy_billable)
 	
-# while cost < ppa_rate:
-# 	cost += cost
-# 	print(cost)
-# 	if cost == ppa_rate:
-# 	break
-# 	print("It makes economic sense to roll a truck.")
+while cost > daily_loss:
+	cost += cost
+	print(cost)
+	# if cost == ppa_rate:
+	# break
+	# print("It makes economic sense to roll a truck.")
