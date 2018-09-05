63c94c30aa23b9f7ab06bec52c7462cc671658ac
tahentx <hendricks.ta@gmail.com>
2018-09-04 18:43:06 -0700

    refactored to reflect pattern provided here: http://interactivepython.org/runestone/static/pip2/Functions/Returningavaluefromafunction.html

--- truckroll.py
+++ truckroll.py
@@ -1,19 +1,24 @@
-cost = int(input("What is your truck roll cost?"))
-# budget = input("What is your annual O&M budget?")
-# PPA units in kWh
-ppa = 0.15
-# Plant capacity in kw
-# plant_dc_capacity = 996.48
-# plant_ac_capacity = 744
-energy_billable = 4000
-def daily_loss(ppa,energy_billable):
-	loss_calculation = int(ppa * energy_billable)
-	print(loss_calculation)
-daily_loss(ppa,energy_billable)
-daily_loss_compound = daily_loss(ppa, energy_billable)
+# cost = int(input("What is your truck roll cost?"))
+# # budget = input("What is your annual O&M budget?")
+# # PPA units in kWh
+# # Plant capacity in kw
+# # plant_dc_capacity = 996.48
+# # plant_ac_capacity = 744
+# energy_billable = 4000
+# def daily_loss(energy_billable):
+# 	loss_calculation = int(ppa * energy_billable)
+# 	return loss_calculation
+# print(daily_loss(ppa,energy_billable)) 
 
-print(type(daily_loss_compound))
-print(type(cost))
+def energy_loss(ppa):
+	loss = ppa * ppa
+	return loss
+
+ppaRialto = .15
+lost_energy = energy_loss(ppaRialto)
+print("The lost energy for Rialto is " + str(lost_energy))
+
+
 # while cost > daily_loss_compound:
 # 	daily_loss_compound += daily_loss_compound
 # 	print(daily_loss_compound)
