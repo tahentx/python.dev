0fb2e1e7fe850b5063db42753f7786aee55fb99e
tahentx <hendricks.ta@gmail.com>
2018-09-05 20:06:10 -0700

    remove extraneous code and capture budget from user

--- truckroll.py
+++ truckroll.py
@@ -1,22 +1,13 @@
-# cost = int(input("What is your truck roll cost?"))
-# # budget = input("What is your annual O&M budget?")
-# # PPA units in kWh
-# # Plant capacity in kw
-# # plant_dc_capacity = 996.48
-# # plant_ac_capacity = 744
-# energy_billable = 4000
-# def daily_loss(energy_billable):
-# 	loss_calculation = int(ppa * energy_billable)
-# 	return loss_calculation
-# print(daily_loss(ppa,energy_billable)) 
+# Capture PPA rate from user
+budget = int(input("What does it cost to roll a truck?"))
 ppaRialto = int(input("What is the PPA rate (please enter whole units?"))/100
 print(ppaRialto)
-# def energy_loss(ppa):
-# 	loss = ppa * ppa
-# 	return loss
+def energy_loss(ppa):
+	loss = ppa * ppa
+	return loss
 
-# lost_energy = energy_loss(ppaRialto)
-# print("The lost energy for Rialto is " + str(lost_energy))
+lost_energy = energy_loss(ppaRialto)
+print("The lost energy for Rialto is " + str(lost_energy))
 
 
 # while cost > daily_loss_compound:
