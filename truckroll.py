5d8403c9e3c1bb4242c35d5858d06e56e6fbe706
tahentx <hendricks.ta@gmail.com>
2018-09-06 17:31:43 -0700

    test random generator

--- truckroll.py
+++ truckroll.py
@@ -1,13 +1,16 @@
 # Capture PPA rate from user
-budget = int(input("What does it cost to roll a truck?"))
-ppaRialto = int(input("What is the PPA rate (please enter whole units?"))/100
-print(ppaRialto)
-def energy_loss(ppa):
-	loss = ppa * ppa
-	return loss
+import random
 
-lost_energy = energy_loss(ppaRialto)
-print("The lost energy for Rialto is " + str(lost_energy))
+print random()
+# budget = int(input("What does it cost to roll a truck?"))
+# ppaRialto = int(input("What is the PPA rate (please enter whole units?"))/100
+# print(ppaRialto)
+# def energy_loss(ppa):
+# 	loss = ppa * ppa
+# 	return loss
+
+# lost_energy = energy_loss(ppaRialto)
+# print("The lost energy for Rialto is " + str(lost_energy))
 
 
 # while cost > daily_loss_compound:
