c53a6ab2bed7ae5086925a3a8f227e78316f17e4
tahentx <hendricks.ta@gmail.com>
2018-09-07 14:32:39 -0700

    write work orders to file

--- cookbook.py
+++ cookbook.py
@@ -1,4 +1,20 @@
 # Ingest Upton inverter data
-with open('workorder.txt', 'r') as f:
- data = f.read()
- print(data)
+# with open('workorder.txt', 'r') as f:
+#  data = f.read()
+#  print(data)
+
+# Write new work order
+wo1 = []
+wo1.append("WO-43528")
+wo1.append("WO-21342")
+wo1.append("WO-88887")
+wo1.append("WO-23955")
+
+file = open("workorder.txt", "r+")
+for i in range(4):
+	file.write(wo1[i])
+	# file.write("\n")
+
+for line in file:
+	print(line)
+file.close()
