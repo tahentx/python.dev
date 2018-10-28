45a7d9cd23fc2b4a60befc156520c5cd5ebd3d5e
tahentx <hendricks.ta@gmail.com>
2018-10-28 16:48:34 -0400

    refactoring the truck roll exercise. need to print opportunity cost value

--- truckroll.py
+++ truckroll.py
@@ -1,21 +1,6 @@
-# Capture PPA rate from user
-import random
-
-print(random())
-# budget = int(input("What does it cost to roll a truck?"))
-# ppaRialto = int(input("What is the PPA rate (please enter whole units?"))/100
-# print(ppaRialto)
-# def energy_loss(ppa):
-# 	loss = ppa * ppa
-# 	return loss
-
-# lost_energy = energy_loss(ppaRialto)
-# print("The lost energy for Rialto is " + str(lost_energy))
-
-
-# while cost > daily_loss_compound:
-# 	daily_loss_compound += daily_loss_compound
-# 	print(daily_loss_compound)
-	# if cost == ppa_rate:
-	# break
-	# print("It makes economic sense to roll a truck.")
+ppa_price = 10
+production_capacity = 250
+def opportunity_cost (ppa_price, production_capacity):
+	opp_cost = ppa_price * production_capacity
+	return opp_cost
+print(str(opportunity_cost))
