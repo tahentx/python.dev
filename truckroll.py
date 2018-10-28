c457e37453d39bae3008fb8c9c93c92bffc3bd16
tahentx <hendricks.ta@gmail.com>
2018-10-28 17:20:59 -0400

    capture user input for variables effceting truck roll analysis

--- truckroll.py
+++ truckroll.py
@@ -1,6 +1,6 @@
-ppa_price = 10
-regional_truck_roll_hourly_cost = 150
-if ppa_price > regional_truck_roll_hourly_cost:
+lost_production = input("What is your daily system PPA loss: ")
+regional_truck_roll_hourly_cost = input("What is your truck roll cost: ")
+if lost_production > regional_truck_roll_hourly_cost:
 	print("Roll a truck.")
 else:
 	print("The economics don't make sense.")  
