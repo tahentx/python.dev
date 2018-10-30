bf626e65941b6115704d428a7673fda3889b1ff3
tahentx <hendricks.ta@gmail.com>
2018-10-29 18:15:49 -0700

    add conditional statement to include parity with truck roll and other variable

--- truckroll.py
+++ truckroll.py
@@ -2,5 +2,7 @@ lost_production = input("What is your daily system PPA loss: ")
 regional_truck_roll_hourly_cost = input("What is your truck roll cost: ")
 if lost_production > regional_truck_roll_hourly_cost:
 	print("Roll a truck.")
+elif lost_production == regional_truck_roll_hourly_cost:
+	print("Jump ball.")
 else:
 	print("The economics don't make sense.")  
