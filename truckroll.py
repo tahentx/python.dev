81fce583053aabc62ffda6f6e3632d82b7d2a516
tahentx <hendricks.ta@gmail.com>
2018-10-30 19:08:56 -0700

    test for loop, one out of three

--- truckroll.py
+++ truckroll.py
@@ -1,10 +1,11 @@
-lost_production = input("What is your daily system PPA loss: ")
-regional_truck_roll_hourly_cost = input("What is your truck roll cost: ")
-if lost_production > regional_truck_roll_hourly_cost:
-	print("Roll a truck.")
-elif lost_production == regional_truck_roll_hourly_cost:
-	print("Jump ball.")
-else:
-	print("The economics don't make sense.")  
+# lost_production = input("What is your daily system PPA loss: ")
+# regional_truck_roll_hourly_cost = input("What is your truck roll cost: ")
+# if lost_production > regional_truck_roll_hourly_cost:
+# 	print("Roll a truck.")
+# elif lost_production == regional_truck_roll_hourly_cost:
+# 	print("Jump ball.")
+# else:
+# 	print("The economics don't make sense.")  
 
-string = "Thick freaky"
+for x in range(0,7):
+	print("What it do.") 
