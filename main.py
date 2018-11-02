66e19408efe9174cce2d3d207f43dc53af14a6b7
tahentx <hendricks.ta@gmail.com>
2018-11-02 15:06:57 -0700

    corrected the api key from 'key' to 'api_key' and removed the extra api key that was appended. it works now.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 
 def main():
-	url = "https://monitoringapi.solaredge.com/sites/list?api_key=XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4"
-	api_key = {'key': 'XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4'}
+	url = "https://monitoringapi.solaredge.com/sites/list?"
+	api_key = {'api_key': '00B0NBZ03D5X517UX7UFJW3PSKFA4BUL'}
 	response = requests.get(url, params=api_key)
 	print(response.text)
 
 if __name__ == "__main__":
 		main()
-
+# api_key=XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4
