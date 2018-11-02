afb7e86e849fddad3710ca6243166a2e4c34c905
tahentx <hendricks.ta@gmail.com>
2018-11-01 20:15:32 -0700

    refactored to pull api key out of url and pass it in via key value store

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 
 def main():
 	url = "https://monitoringapi.solaredge.com/sites/list?api_key=XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4"
-	response = requests.get(url)
+	api_key = {'key': 'XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4'}
+	response = requests.get(url, params=api_key)
 	print(response.text)
 
 if __name__ == "__main__":
