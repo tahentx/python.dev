a3f1acd53ca629d053945f201cd0cd642dc5c12c
tahentx <hendricks.ta@gmail.com>
2018-11-02 20:52:10 -0700

    import json module to convert response to json format

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import requests
+import json
 
 def main():
 	url = "https://monitoringapi.solaredge.com/sites/list?"
-	api_key = {'api_key': '00B0NBZ03D5X517UX7UFJW3PSKFA4BUL'}
-	response = requests.get(url, params=api_key)
-	print(response.text)
+	api_key = {'api_key': 'XO272VFG4MWNJM1LTOSX6G0E2OUA19XA'}
+
+	site_metadata = requests.get(url, params=api_key)
+	print(site_metadata.text)
+
 
 if __name__ == "__main__":
 		main()
 # api_key=XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4
+
