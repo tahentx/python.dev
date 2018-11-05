503d58516ad98df2412d4987999302ba5d06c093
tahentx <hendricks.ta@gmail.com>
2018-11-05 14:13:35 -0800

    minor tweak to accomodate solar log api

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import dpath.util
 # dpath might be the library to use to pull data from complex dictionary
 # url and api key to test solaredge api
 def main():
-	url = "https://monitoringapi.solaredge.com/sites/list?"
-	api_key = {'api_key': 'XO272VFG4MWNJM1LTOSX6G0E2OUA19XA'}
+	url = "http://demo.solarlog-web.net/api?username=812045917&password=ufntoedz&solarlog=9&function=getMinuteValues&format=json&date"
+	# api_key = {'api_key': 'XO272VFG4MWNJM1LTOSX6G0E2OUA19XA'}
 # first use of requests library
-	response = requests.get(url, params=api_key)
-	data = json.loads(response.text)
-	project = data['sites']['site']
-	for data in project:
-		print(project['modelName'])
+	response = requests.get(url)
+	print(response.text)
+	# data = json.loads(response.text)
+	# project = data['sites']['site']
+	# for data in project:
+	# 	print(project['modelName'])
 
 if __name__ == "__main__":
 		main()
