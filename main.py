eefcaed85ac4791ae2405d3597b5db7744c17673
tahentx <hendricks.ta@gmail.com>
2018-11-05 18:40:46 -0800

    page 24 of coding exercises book

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import dpath.util
 # dpath might be the library to use to pull data from complex dictionary
 # url and api key to test solaredge api
 def main():
-	url = "http://demo.solarlog-web.net/api?username=812045917&password=ufntoedz&solarlog=9&function=getMinuteValues&format=json&date"
-	# api_key = {'api_key': 'XO272VFG4MWNJM1LTOSX6G0E2OUA19XA'}
+	url = "https://monitoringapi.solaredge.com/site/581140/energy?timeUnit=QUARTER_OF_AN_HOUR&startDate=2018-10-23&endDate=2018-10-24&api_key=XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4"
+	api_key = {'api_key': 'XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4'}
 # first use of requests library
-	response = requests.get(url)
+	response = requests.get(url,params=api_key)
 	print(response.text)
 	# data = json.loads(response.text)
 	# project = data['sites']['site']
---
+++ self_checkout.py
@@ -0,0 +1,7 @@
+
+int(a) = input("Enter the price of item 1:")
+int(b) = input("Enter the quantity of item 1:")
+def multiply(a,b):
+	return a*b
+multiply(a,b)
+
