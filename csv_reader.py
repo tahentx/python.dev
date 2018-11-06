489e5f9fcc99b49d05ea74e8d9fb8dae1156dfb7
tahentx <hendricks.ta@gmail.com>
2018-11-05 18:57:28 -0800

    create csv reader using this tutorial for reference https://www.youtube.com/watch?v=q5uM4VKywbA&index=15&list=PL-osiE80TeTskrapNbzXhwoFUiLCjGgY7

---
+++ csv_reader.py
@@ -0,0 +1,4 @@
+import csv
+
+with open('maxgenusers.csv', 'r') as csv_file:
+	csv_reader = csv.reader(csv_file)
--- self_checkout.py
+++ self_checkout.py
@@ -1,6 +1,5 @@
-
-int(a) = input("Enter the price of item 1:")
-int(b) = input("Enter the quantity of item 1:")
+a = input("Enter the price of item 1:")
+b = input("Enter the quantity of item 1:")
 def multiply(a,b):
 	return a*b
 multiply(a,b)
