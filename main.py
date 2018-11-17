e9f1c5accf44588732a608cca2a38ac02a0b2d63
tahentx <hendricks.ta@gmail.com>
2018-11-17 10:38:05 -0800

    minor modifaction to the data being returned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import dpath.util
+
 # dpath might be the library to use to pull data from complex dictionary
 # url and api key to test solaredge api
 def main():
@@ -8,8 +8,7 @@ def main():
 	api_key = {'api_key': 'XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4'}
 # first use of requests library
 	response = requests.get(url,params=api_key)
-	print(response.text)
-	
+	print(response)
 	
 	
 
