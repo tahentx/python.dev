7bbd63003af7ff155efbcdf37b2c0b98f30637dd
tahentx <hendricks.ta@gmail.com>
2018-11-17 10:58:51 -0800

    testing smartsheets api for pulling onyx project metadata. interesting that response.text returns html. next step is find out how to return the data in json or develop a parsing script

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import json
 # dpath might be the library to use to pull data from complex dictionary
 # url and api key to test solaredge api
 def main():
-	url = "https://monitoringapi.solaredge.com/site/581140/energy?timeUnit=QUARTER_OF_AN_HOUR&startDate=2018-10-23&endDate=2018-10-24&api_key=XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4"
-	api_key = {'api_key': 'XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4'}
+	url = "https://app.smartsheet.com/b/home?lx=GYHFpeSnjeyOZXV5k2SRGA"
+	api_key = {'api_key': 'j3ashfc8ezwkan27jger2l4ewx'}
 # first use of requests library
 	response = requests.get(url,params=api_key)
-	print(response)
+	print(response.text)
 	
 	
 
