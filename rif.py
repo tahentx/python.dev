0d3722b748f45995150a25edb65313222dbbb465
tahentx <hendricks.ta@gmail.com>
2018-11-17 12:21:19 -0800

    delete unnecessary print statement

--- rif.py
+++ rif.py
@@ -1,11 +1,12 @@
 employees = ['Bob','Joe','Bill','Mike','Steve','Matt']
-rif = input("Who is terminated? ")
-confirm = input("We need to terminate " + rif + ", is that correct? Y or N. \n")
-if confirm == 'Y':
-	print("Ok. We will terminate " + rif + ".")
-else:
-	print("Ok. He can stay.")
-for x in rif:
-	if rif in employees:
-		print(rif + " has been terminated.")
-		del rif
+headcount = len(employees)
+rif = input("Your headcount is " + str(headcount) + ". Who is to be terminated? ")
+# confirm = input("We need to terminate " + rif + ", is that correct? Y or N. \n")
+# if confirm == 'Y':
+# 	print("Ok. We will terminate " + rif + ".")
+# else:
+# 	print("Ok. He can stay.")
+for x in employees:
+	if x in employees == rif:
+		print("test")
+		
