d4b0b6029a25f3a64189bfb676ce1bf9964a9f05
tahentx <hendricks.ta@gmail.com>
2018-11-29 20:03:10 -0800

    extract text successfully

--- pdfreader.py
+++ pdfreader.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import PyPDF2
-pdfFileObj = open('02 - As-Built Drawings.pdf', 'rb')
+pdfFileObj = open('SCADA System Fundamentals.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-print(pdfReader.numPages) 
+pageObj = pdfReader.getPage(0)
+print(pageObj.extractText()) 
