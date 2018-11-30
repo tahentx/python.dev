ad69375b200065ffd97e42c6199fcec9426bef8c
tahentx <hendricks.ta@gmail.com>
2018-11-29 19:50:36 -0800

    print the number of pages for the pdf using pypdf2

--- pdfreader.py
+++ pdfreader.py
@@ -2,3 +2,6 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import PyPDF2
+pdfFileObj = open('02 - As-Built Drawings.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+print(pdfReader.numPages) 
