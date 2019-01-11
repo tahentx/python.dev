c0e9bbbab960c0e2d47478de26bd3381c854454a
tahentx <hendricks.ta@gmail.com>
2019-01-10 18:20:02 -0800

    add request snippet and required parameters for distance api

---
+++ distance.js
@@ -0,0 +1,10 @@
+lat1 = 33.850455
+long1 = -118.140378
+lat2 = 34.184308
+long2 = -118.888122
+
+unirest.get("https://010pixel-distance-v1.p.rapidapi.com/?unit=K&lat1=10&long1=-25.3&lat2=34.5&long2=-403.4")
+.header("X-RapidAPI-Key", "54bfa778e0msh52ed7af4b6f98cfp18b709jsnbc0ccec09a9c")
+.end(function (result) {
+  console.log(result.status, result.headers, result.body);
+});
