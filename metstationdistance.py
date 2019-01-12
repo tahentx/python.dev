ce608e046d36e1d9d66bd70696ce98f8d714d605
tahentx <hendricks.ta@gmail.com>
2019-01-11 18:56:02 -0800

    python version of distance

---
+++ metstationdistance.py
@@ -0,0 +1,9 @@
+import unirest
+
+
+
+response = unirest.get("https://010pixel-distance-v1.p.rapidapi.com/?unit=K&lat1=10&long1=-25.3&lat2=34.5&long2=-403.4",
+  headers={
+    "X-RapidAPI-Key": "54bfa778e0msh52ed7af4b6f98cfp18b709jsnbc0ccec09a9c"
+  }
+)
