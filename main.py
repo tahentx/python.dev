4989749132c94dfc7e00476194ded8cc5a65ae26
tahentx <hendricks.ta@gmail.com>
2019-01-16 18:05:11 -0800

    modify file to test connection to server at new skyview site

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import json
 # dpath might be the library to use to pull data from complex dictionary
 # url and api key to test solaredge api
 def main():
-	url = "https://app.smartsheet.com/b/home?lx=GYHFpeSnjeyOZXV5k2SRGA"
-	api_key = {'api_key': 'j3ashfc8ezwkan27jger2l4ewx'}
+	url = "https://monitoringapi.solaredge.com/site/581140/energy?timeUnit=QUARTER_OF_AN_HOUR&startDate=2018-10-23&endDate=2018-10-24&api_key=XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4"
+	api_key = {'api_key': 'XGTHLV7EGP2EM1UTP7FWIY6PL9NKDT7W'}
 # first use of requests library
 	response = requests.get(url,params=api_key)
-	print(response.text)
+	print(response.status_code)
 	
 	
 
 if __name__ == "__main__":
 		main()
-# api_key=XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4
+
 
