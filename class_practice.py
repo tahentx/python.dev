b3ce1315cd10ae595cf2d55336afcde20864ca9d
tahentx <hendricks.ta@gmail.com>
2019-01-26 15:35:40 -0800

    instantiate all the necessary objects and assign values to their properties

--- class_practice.py
+++ class_practice.py
@@ -1,4 +1,6 @@
 import csv
+import geopy.distance
+
 with open('sd_metstations.csv', 'r') as csv_file:
 	met = csv.reader(csv_file)
 	metlist = list(met)
@@ -9,10 +11,32 @@ class Site:
 		self.lat = ""
 		self.long = ""
 
-ironmountain = Site(metlist[0][1],metlist[1][2])
+def calculate(coords_1,coords_2):
+	coords_1 = ""
+	coords_2 = ""
+	return geopy.distance.distance(coords_1, coords_2).miles
+
+ironmountain = Site(metlist[0][1],metlist[0][2])
 ironmountain.lat = metlist[0][1]
-ironmountain.long = metlist[1][2]
-print("The latitude of Iron Mountain Flanders is " + str(ironmountain.lat) + "and the longitude is " + str(ironmountain.long))
+ironmountain.long = metlist[0][2]
+sdpd_west = Site(metlist[1][1],metlist[1][2])
+sdpd_west.lat = metlist[1][1]
+sdpd_west.long = metlist[1][2]
+sd_missionvalley = Site(metlist[2][1],metlist[2][2])
+sd_missionvalley.lat = metlist[2][1]
+sd_missionvalley.long = metlist[2][2]
+sdpd_central = Site(metlist[3][1],metlist[3][2])
+sdpd_central.lat = metlist[3][1]
+sdpd_central.long = metlist[3][2]
+mission_trails = Site(metlist[4][1],metlist[4][2])
+mission_trails.lat = metlist[4][1]
+mission_trails.long = metlist[4][2]
+inspiration = Site(metlist[5][1],metlist[5][2])
+inspiration.lat = metlist[5][1]
+inspiration.long = metlist[5][2]
+print(inspiration.lat)
+
+
 # ironmountain = Site(met[1:],met[2])
 # ironmountain.lat = met[1]
 # print(ironmountain.lat)
