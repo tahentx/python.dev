7c63c91d8507da9587c96d5f2342757d1d6d9653
tahentx <hendricks.ta@gmail.com>
2019-01-26 15:03:22 -0800

    successfully grab indices from the csv. praise the lord.

--- class_practice.py
+++ class_practice.py
@@ -9,8 +9,10 @@ class Site:
 		self.lat = ""
 		self.long = ""
 
-print(metlist)
-
+ironmountain = Site(metlist[0][1],metlist[1][2])
+ironmountain.lat = metlist[0][1]
+ironmountain.long = metlist[1][2]
+print("The latitude of Iron Mountain Flanders is " + str(ironmountain.lat) + "and the longitude is " + str(ironmountain.long))
 # ironmountain = Site(met[1:],met[2])
 # ironmountain.lat = met[1]
 # print(ironmountain.lat)
