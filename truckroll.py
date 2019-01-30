0494655c997192b0547f8399b6ffad42df69d59d
tahentx <hendricks.ta@gmail.com>
2019-01-29 21:07:56 -0800

    generate random kWh loss and random equipment offline

--- truckroll.py
+++ truckroll.py
@@ -1,3 +1,6 @@
 from random import *
 outage = randint(1,1000)
-print(outage)
+
+equipment = ['combiner','tracker','inverter', 'transformer', 'breaker']
+equipment_offline = sample(equipment, 1)
+print('Event: ' + str(equipment_offline[0]) + ' failure today; estimated lost kWh: ' + str(outage) + '.')
