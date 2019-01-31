9368e523a58c8ccfe8dc37652aca23add1f9ce53
tahentx <hendricks.ta@gmail.com>
2019-01-30 17:32:41 -0800

    roll random event generator into function

--- truckroll.py
+++ truckroll.py
@@ -2,5 +2,9 @@ from random import *
 outage = randint(1,1000)
 
 equipment = ['combiner','tracker','inverter', 'transformer', 'breaker']
-equipment_offline = sample(equipment, 1)
-print('Event: ' + str(equipment_offline[0]) + ' failure today; estimated lost kWh: ' + str(outage) + '.')
+
+def random_outage_generator(equipment):
+	equipment_offline = sample(equipment, 1)
+	print('Event: ' + str(equipment_offline[0]) + ' failure today; estimated lost kWh: ' + str(outage) + '.')
+
+random_outage_generator(equipment)
