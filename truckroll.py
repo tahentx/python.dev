69b8461f756da338f8b60baa7ee95baf475f6686
tahentx <hendricks.ta@gmail.com>
2019-01-30 17:40:32 -0800

    add pv watts file

--- truckroll.py
+++ truckroll.py
@@ -1,5 +1,12 @@
+import csv
 from random import *
 outage = randint(1,1000)
+
+with open('Academy pvwatts_hourly', 'r') as csv_file:
+	model = csv.reader(csv_file)
+	model_list = list(model)
+
+print(model_list)
 
 equipment = ['combiner','tracker','inverter', 'transformer', 'breaker']
 
@@ -7,4 +14,4 @@ def random_outage_generator(equipment):
 	equipment_offline = sample(equipment, 1)
 	print('Event: ' + str(equipment_offline[0]) + ' failure today; estimated lost kWh: ' + str(outage) + '.')
 
-random_outage_generator(equipment)
+# random_outage_generator(equipment)
