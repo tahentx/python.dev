2da910b4c4b0d5e608bfdb869efbedd2a8db10f9
tahentx <hendricks.ta@gmail.com>
2019-02-06 22:23:46 -0600

    add coordinates list

--- pvlib_beta.py
+++ pvlib_beta.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-naive_times = pd.DatetimeIndex(start='2017', end='2018', freq='1h')
+naive_times = pd.date_range(start='2017', end='2018', freq='1h')
+coordinates = [(30, -110, 'Tucson', 700, 'Etc/GMT+7'),
+                   (40, -120, 'San Francisco', 10, 'Etc/GMT+8'),
+                   (35, -105, 'Albuquerque', 1500, 'Etc/GMT+7'),
+                   (50, 10, 'Berlin', 34, 'Etc/GMT-1')]
 
 print(naive_times)
+print(coordinates[0])
